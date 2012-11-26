e2338d7181543cdcbf9593e68f608404b1b95aec
palador <palador@projektangriff.de>
2012-11-26 23:11:32 +0100

    Revision Collection: removed all collection types but lists for easy experiments

--- src/main/java/org/pa/lang/collection/revision/SlaveMap.java
+++
@@ -1,6 +0,0 @@
-package org.pa.lang.collection.revision;
-
-import java.util.Map;
-
-public interface SlaveMap<K, V> extends RevisionControlSlave, Map<K, V> {
-}
--- src/main/java/org/pa/lang/collection/revision/SlaveSet.java
+++
@@ -1,6 +0,0 @@
-package org.pa.lang.collection.revision;
-
-import java.util.Set;
-
-public interface SlaveSet<E> extends SlaveCollection<E>, Set<E> {
-}
